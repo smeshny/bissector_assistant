f4cd334cc65bf3b4a6ffbef4ea886a32dc762834
smeshny <smeshny@gmail.com>
2017-11-22 23:17:24 +0300

    added replyKeybordMenu

--- bissector_bot.py
+++ bissector_bot.py
@@ -19,6 +19,15 @@ async def send_welcome(message: types.Message):
                             "\nПока что. \nСкоро стану скайнетом")
 
 
+@dp.message_handler(commands=['menu'])
+async def add_menu(message: types.Message):
+
+        miners = types.KeyboardButton("/miners")
+        balance = types.KeyboardButton("/balance")
+        markup = types.ReplyKeyboardMarkup([[miners, balance]], True, False)
+
+        await bot.send_message(chat_id=message.chat.id, text="Жми!", reply_markup=markup)
+
 @dp.message_handler(commands=['miners'])
 async def check_miners(message: types.Message):
         await bot.send_message(chat_id=message.chat.id, text=check_status_ethermine(), parse_mode='Markdown')
